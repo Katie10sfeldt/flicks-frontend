2206e84016deda6ff00f63225539bbfc5f7962c7
Katie Tensfeldt <kmtensfeldt@gmail.com>
2020-08-06 13:12:53 -0700

    Add first part of story element

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,10 @@
+// .storybook/main.js
+
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: ['../src/components/**/*.stories.js'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
   ],
 };
-
