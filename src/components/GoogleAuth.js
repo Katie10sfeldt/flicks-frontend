11a48ba805a2d48da02a80742bbdf081f19ddcab
Katie Tensfeldt <kmtensfeldt@gmail.com>
2020-11-20 15:08:49 -0800

    Show user info in alert.

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -12,10 +12,12 @@ class GoogleAuth extends React.Component {
 				.init({
 					clientId:
 						'468373991800-8m7kjs4or7icm4ubobkc9i2bommmerdt.apps.googleusercontent.com',
-					scope: 'email',
+					scope:
+						'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
 				})
 				.then(() => {
 					this.auth = window.gapi.auth2.getAuthInstance();
+					// window.gapi.auth2.get.BasicProfile();
 					this.onAuthChange();
 					this.auth.isSignedIn.listen(this.onAuthChange);
 				});
@@ -27,7 +29,8 @@ class GoogleAuth extends React.Component {
 	};
 
 	onSignIn = () => {
-		this.auth.signIn();
+		const info = this.auth.signIn();
+		console.log(info);
 	};
 
 	onSignOut = () => {
@@ -45,6 +48,14 @@ class GoogleAuth extends React.Component {
 				/>
 			);
 		} else if (this.state.isSignedIn) {
+			const profile = this.auth.currentUser.get().getBasicProfile();
+			alert('ID: ' + profile.getId());
+			alert('Full Name: ' + profile.getName());
+			alert('Given Name: ' + profile.getGivenName());
+			alert('Family Name: ' + profile.getFamilyName());
+			alert('Email: ' + profile.getEmail());
+			alert('ImageURL: ' + profile.getImageUrl());
+
 			return (
 				<SignUpButton
 					button={{
@@ -75,9 +86,3 @@ class GoogleAuth extends React.Component {
 }
 
 export default GoogleAuth;
-
-// access_token: "ya29.A0AfH6SMAEnXQc4NFola0AZIgrLc1oS5IGkadnzRyI1IgJydrLGlrBhPbIBXjZaUFSnuTJylA30rAvKb7QRTLoHr_UX9JmTEJnCrC7bs6RRp8o8-mjYInHp--P0FOXYkRhTbMpIgZjKfw2K7GvtAKh9IY1BNxtmDSDdfhgsIF1MtaZ"
-// expires_in: 3599
-// id_token: "eyJhbGciOiJSUzI1NiIsImtpZCI6ImYwOTJiNjEyZTliNjQ0N2RlYjEwNjg1YmI4ZmZhOGFlNjJmNmFhOTEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiNDY4MzczOTkxODAwLThtN2tqczRvcjdpY200dWJvYmtjOWkyYm9tbW1lcmR0LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiNDY4MzczOTkxODAwLThtN2tqczRvcjdpY200dWJvYmtjOWkyYm9tbW1lcmR0LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTA0NzE0NjAwMTg5NzM1NDY2MTg1IiwiZW1haWwiOiJrbXRlbnNmZWxkdEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6ImpDN1A5X3NOclZSc1RDUnk0ang2SkEiLCJuYW1lIjoiS2F0aGxlZW4gVGVuc2ZlbGR0IiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS8tRlpfQmc4RTVFX3MvQUFBQUFBQUFBQUkvQUFBQUFBQUFBQUEvQU1adXVja2Y2dEdIeS0ydkdWRUhZTWgtVWFyWmk2Q21JQS9zOTYtYy9waG90by5qcGciLCJnaXZlbl9uYW1lIjoiS2F0aGxlZW4iLCJmYW1pbHlfbmFtZSI6IlRlbnNmZWxkdCIsImxvY2FsZSI6ImVuIiwiaWF0IjoxNjA0Njg3MTM3LCJleHAiOjE2MDQ2OTA3MzcsImp0aSI6IjU4YTk3MWYwMDVkZmExM2Y5ODZjOGI2MTYwZjYyZTMwMDI2NWZiMjEifQ.rWExRQx2kvFSZK357j9Ibv_a6Zq4gaom5hNFjhOXbFR3twW4tJrGrRbE6LpPnuXFZTlFkCu2HyilpgPnQXYQZWGUNKI7hcNhT1gsgnr2LtFJNr2x_8Yi3-H1X3IRaujt1dt-cFQY7frTTlSxXBcbz90yMYROJUWzgTojuwh9pui2Dao8XoY1wfwocyMenxp4ItyBWwpWDIwwAy_lAP9SfU4BynrearF_E88TTsdV9M9ByL0ui89h8aR7zJPQIcpIxXrzzzuuKQibHBN2pOdUqzbCVjPKICYpPVV2YXs72k6JJHr86ajiotFYB-LWxSgXhhpE4U85IWHu1T5RuQqgaw"
-// login_hint: "AJDLj6JUa8yxXrhHdWRHIV0S13cAPBdmzZOI-_0zN78rYM8TtXpgW4Tm4d8lauA287giwhcf6659bJ75gTa6oGHsH-vjNLBMiw"
-// scope: "email profile https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email openid"
